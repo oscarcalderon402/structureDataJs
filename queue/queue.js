8840d33ca91f7dc03aafaf847532823fee8a295f
oscarcalderon402 <gregoriocalderon.07@hotmail.es>
2022-03-29 09:25:36 -0400

    queue

---
+++ queue/queue.js
@@ -0,0 +1,50 @@
+// enqueue: agregar un elemento al final de la linea
+// dequeue: remover al primer elemento de la linea
+// peek: tomar el primer elemento de la linea
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+
+  peek() {
+    return this.first;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+
+    return this;
+  }
+  dequeue() {
+    if (!this.first) {
+      return null;
+    }
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.length--;
+
+    return this;
+  }
+}
+
+const myQueue = new Queue();
